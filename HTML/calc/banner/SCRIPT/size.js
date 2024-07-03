8961e70f6e37d67daaa58f46eb9e9a49046d6dc3
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-07-03 12:06:43 +0300

    1

---
+++ HTML/calc/banner/SCRIPT/size.js
@@ -0,0 +1,18 @@
+/* Получение значения input высоты и ширины */
+var banner_size = document.body.querySelector('.size');
+var banner_width_input = banner_size.children[2];
+var banner_height_input = banner_size.children[1];
+
+/* Изменение высоты холста */
+banner_height_input.oninput = function() {
+    var banner_width  = banner_width_input.value;
+    var banner_height = banner_height_input.value;
+    block.style.aspectRatio = `${banner_width}/${banner_height}`;
+};
+
+/* Изменение ширины холста */
+banner_width_input.oninput = function() {
+    var banner_width  = banner_width_input.value;
+    var banner_height = banner_height_input.value;
+    block.style.aspectRatio = `${banner_width}/${banner_height}`;
+};
